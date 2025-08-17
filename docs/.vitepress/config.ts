a56ea1c8cf355c57d68f5137b7f9324fa64dd7d9
Meiyu <130171001+MeiyuRain@users.noreply.github.com>
2025-08-17 21:17:49 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,10 +1,7 @@
-import { basename } from 'node:path'
 import { defineConfig } from 'vitepress'
 import MarkdownPreview from 'vite-plugin-markdown-preview'
 
 import { head, nav, sidebar } from './configs'
-
-const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
 
 export default defineConfig({
   outDir: '../dist',
@@ -18,33 +15,24 @@ export default defineConfig({
   lastUpdated: true,
   cleanUrls: true,
 
-  /* markdown 配置 */
   markdown: {
     lineNumbers: true,
   },
 
-  /* 主题配置 */
   themeConfig: {
     i18nRouting: false,
-
     logo: '/logo.png',
-
     nav,
     sidebar,
-
-    /* 右侧大纲配置 */
     outline: {
       level: 'deep',
       label: '目录',
     },
-
     socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996/vitepress-nav-template' }],
-
     footer: {
       message: '如有转载或 CV 的请标注本站原文地址',
       copyright: 'Copyright © 2019-present maomao',
     },
-
     lastUpdated: {
       text: '最后更新于',
       formatOptions: {
@@ -52,23 +40,18 @@ export default defineConfig({
         timeStyle: 'medium',
       },
     },
-
     docFooter: {
       prev: '上一篇',
       next: '下一篇',
     },
-
     returnToTopLabel: '回到顶部',
     sidebarMenuLabel: '菜单',
     darkModeSwitchLabel: '主题',
     lightModeSwitchTitle: '切换到浅色模式',
     darkModeSwitchTitle: '切换到深色模式',
-
-    /*** 自定义配置 ***/
     visitor: {
       badgeId: 'maomao1996.vitepress-nav-template',
     },
-
     comment: {
       repo: 'maomao1996/vitepress-nav-template',
       repoId: 'R_kgDOJC09Jg',
