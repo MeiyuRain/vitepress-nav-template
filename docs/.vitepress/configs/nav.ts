ff20b690ca2529ab59d52f42ac66a2928773bb2a
Meiyu <130171001+MeiyuRain@users.noreply.github.com>
2025-08-17 22:06:25 +0800

    Update nav.ts

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -2,7 +2,7 @@ import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
   { text: '前端导航', link: '/nav/' },
-  { text: '茂茂主页', link: 'https://fe-mm.com' },
+  { text: 'Meiyu主页', link: 'https://blog.meiyukj.cn' },
   {
     text: '茂茂物语',
     link: 'https://notes.fe-mm.com',
